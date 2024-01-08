f72645f659bbb3900e201e9d8128de8033da27c3
dtrongnam <daotrongnam222@gmail.com>
2024-01-09 01:49:02 +0700

    Order API

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ const dotenv = require('dotenv');
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const bodyParser = require('body-parser')
-var cors = require('cors')
-
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(cors());
+app.get('/',(req,res) => {
+    res.send('Hello World')
 
+})
 app.use(bodyParser.json())
 
 routes(app);
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const productController = require('../controllers/ProductController');
+const { authMiddleWare } = require("../middleware/authMiddleware");
 
-router.get('/', productController.getAllProduct)
-router.get('/:id', productController.getProduct)
-router.delete('/:id', productController.deleteProduct)
-router.post('/', productController.createProduct)
-router.patch('/', productController.updateProduct)
+router.get('/', authMiddleWare, productController.getAllProduct)
+router.get('/:id', authMiddleWare, productController.getProduct)
+router.delete('/:id', authMiddleWare, productController.deleteProduct)
+router.post('/', authMiddleWare, productController.createProduct)
+router.patch('/', authMiddleWare, productController.updateProduct)
 
 module.exports = router
